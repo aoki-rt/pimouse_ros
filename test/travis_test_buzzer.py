3bb598ebe9cb47fec534fe2b46c9beba837af767
aoki-rt <aoki@rt-net.jp>
2020-05-22 12:05:05 +0900

    mod travis_test_buzzer

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -13,7 +13,7 @@ class BuzzerTest(unittest.TestCase):
 
     def test_put_value(self):
         pub = rospy.Publisher('/buzzer',UInt16)
-        for i in rangre(10):
+        for i in range(10):
             pub.publish(1234)
             time.sleep(0.1)
 
