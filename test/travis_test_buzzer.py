514cbbdc52189b63d57e32932185d38587aa81f9
masatake aoki <aoki@rt-net.jp>
2020-07-16 11:16:59 +0900

    mod travis_test_buzzer.py

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -4,7 +4,7 @@ import rosnode
 import time
 
 from std_msgs.msg import UInt16
-from pimouse_ros.msg import MusicAction, MusicResult, MusicFeedback,MusicGoal
+from pimouse_ros.msg import MusicAction, MusicResult, MusicFeedback, MusicGoal
 
 class BuzzerTest(unittest.TestCase):
     def setUp(self):
